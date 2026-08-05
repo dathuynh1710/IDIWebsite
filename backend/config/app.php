ea37b140e0869b13d1de228dd968f7b60cbf46e3
Đạt Huỳnh <huynhthanhdat2506@gmail.com>
2026-08-05 23:12:19 +0700

    chore: update timezone settings

--- backend/config/app.php
+++ backend/config/app.php
@@ -61,11 +61,11 @@ return [
     |
     | Here you may specify the default timezone for your application, which
     | will be used by the PHP date and date-time functions. The timezone
-    | is set to "UTC" by default as it is suitable for most use cases.
+    | is configured through APP_TIMEZONE and defaults to Vietnam time.
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => env('APP_TIMEZONE', 'Asia/Ho_Chi_Minh'),
 
     /*
     |--------------------------------------------------------------------------
